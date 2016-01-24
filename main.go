d63deb6625e1bb0c4573aba5eb80f92fa53a963d
javo05 <javierlg.0507@gmail.com>
2016-01-24 14:48:57 -0600

    Add token validation functionality
    The functionality was written as method in order to
    invoke it from any service/program

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	_"fmt"
+	"encoding/json"
+	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/fatih/structs"
 	"github.com/gin-gonic/gin"
 	"github.com/gophergala2016/golin/login"
 	"github.com/gophergala2016/golin/tokens"
+	"regexp"
+	"strings"
 	"time"
 )
 
@@ -50,7 +53,7 @@ func (t Token) GenerateToken(SignatureStr string) (string, error) {
 	} else {
 		return "", err
 	}
-    //TODO Storage
+	//TODO Storage
 }
 
 func main() {
@@ -61,6 +64,37 @@ func main() {
 	privates.GET("/users/:id", GetUser)
 	publics.POST("/users", LoginUser)
 	r.Run(":8080")
+}
+
+func ValidateToken(encriptedToken string) (string, error) {
+	tokData := regexp.MustCompile(`\s*$`).ReplaceAll([]byte(encriptedToken), []byte{})
+
+	currentToken, err := jwt.Parse(string(tokData), func(t *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+
+	// Print an error if we can't parse for some reason
+	if err != nil {
+		fmt.Println("Couldn't parse token: ", err)
+		return "", fmt.Errorf("Couldn't parse token: %v", err)
+	}
+
+	fmt.Println("currentToken ", currentToken)
+	// Is token invalid?
+	if !currentToken.Valid {
+		return "", fmt.Errorf("Token is invalid")
+	}
+
+	if err != nil {
+		//Validate if token has expired or is not parseable
+		//Validate in the blacklist
+		return "", err
+	}
+
+	// Print the token details
+	_, err = json.MarshalIndent(currentToken.Claims, "", "    ")
+
+	return string(tokData), err
 }
 
 func LoginUser(c *gin.Context) {
@@ -89,5 +123,18 @@ func LoginUser(c *gin.Context) {
 
 func GetUser(c *gin.Context) {
 	_ = c.Params.ByName("id")
-	c.JSON(200, gin.H{"message": "chingon perron"})
+	var currentToken string
+
+	authorization_array := c.Request.Header["Authorization"]
+	if len(authorization_array) > 0 {
+		currentToken = strings.Fields(strings.TrimSpace(authorization_array[0]))[1]
+	}
+
+	newToken, err := ValidateToken(currentToken)
+
+	if err != nil {
+		c.JSON(404, gin.H{"error": err})
+	} else {
+		c.JSON(200, gin.H{"message": "chingon perron", "token": newToken})
+	}
 }
